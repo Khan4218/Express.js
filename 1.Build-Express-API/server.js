5d8debdc7e2a3f90cf6493c3bb2d3f60d736bae4
Khan4218 <mirakber4@gmail.com>
2025-10-11 05:32:11 -0500

    serving data

--- 1.Build-Express-API/server.js
+++ 1.Build-Express-API/server.js
@@ -1,7 +1,15 @@
 import express from "express";
+import { startups } from "./data/data.js";
 
 const PORT = 8000;
+
 const app = express()
+
+app.get('/api', (req, res) => {
+  res.json(startups)
+})
+
 app.listen(PORT, () => {
   console.log(`server listening on port:${PORT}`);
 })
+
