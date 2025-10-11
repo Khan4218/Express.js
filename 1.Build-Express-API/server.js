cc24e45bbb9e5ef8cec0fe5e2ded68e54e8d0957
Khan4218 <mirakber4@gmail.com>
2025-10-11 06:33:15 -0500

    Filtering by query params

--- 1.Build-Express-API/server.js
+++ 1.Build-Express-API/server.js
@@ -6,8 +6,34 @@ const PORT = 8000;
 const app = express()
 
 app.get('/api', (req, res) => {
-  console.log(req.query);
-  res.json(startups)
+
+  let filteredData = startups
+
+  const { industry, country, continent, is_seeking_funding, has_mvp } = req.query;
+
+  if (industry) {
+    filteredData = filteredData.filter(f => f.industry.toLowerCase() === industry.toLowerCase())
+  }
+
+  if (country) {
+    filteredData = filteredData.filter(f => f.country.toLowerCase() === country.toLowerCase())
+  }
+
+  if (continent) {
+    filteredData = filteredData.filter(f => f.continent.toLowerCase() === continent.toLowerCase())
+  }
+
+  if (is_seeking_funding) {
+    const seekingFundingBoo = is_seeking_funding === 'true'
+    filteredData = filteredData.filter(f => f.is_seeking_funding === seekingFundingBoo)
+  }
+
+  if (has_mvp) {
+    const mvpBoo = has_mvp === 'true'
+    filteredData = filteredData.filter(f => f.has_mvp === mvpBoo)
+  }
+
+  res.json(filteredData)
 })
 
 
