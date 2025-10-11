ed5fd1eb91af1e14db5e1345674b8253ef72b838
Khan4218 <mirakber4@gmail.com>
2025-10-11 05:38:55 -0500

    query parameters

--- 1.Build-Express-API/server.js
+++ 1.Build-Express-API/server.js
@@ -6,8 +6,10 @@ const PORT = 8000;
 const app = express()
 
 app.get('/api', (req, res) => {
+  console.log(req.query);
   res.json(startups)
 })
+
 
 app.listen(PORT, () => {
   console.log(`server listening on port:${PORT}`);
