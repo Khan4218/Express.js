451732f4cf3a095bad646a741d94526625877e10
Khan4218 <mirakber4@gmail.com>
2025-10-12 07:46:49 -0500

    Route not found

--- 1.Build-Express-API/server.js
+++ 1.Build-Express-API/server.js
@@ -8,8 +8,9 @@ const app = express()
 app.use('/api', apiRouter);
 
 app.use((req, res) => {
-  res.status(401).json("Invalid Path")
-});
+  res.status(404).json({ message: "Endpoint not found. Please check the API documentation." })
+  return
+})
 
 app.listen(PORT, () => {
   console.log(`server listening on port:${PORT}`)
