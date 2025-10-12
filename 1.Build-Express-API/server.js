6bcd08b34a8e721bb0a3858d279107bd51a9bc23
Khan4218 <mirakber4@gmail.com>
2025-10-12 07:52:52 -0500

    CORS

--- 1.Build-Express-API/server.js
+++ 1.Build-Express-API/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { apiRouter } from "./router/apiRouter.js";
+import cors from cors;
 
 const PORT = 8000;
 
 const app = express()
+
+app.use(cors);
 
 app.use('/api', apiRouter);
 
