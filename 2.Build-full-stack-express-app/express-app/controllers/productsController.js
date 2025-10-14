967f479a33a5a03f81acfc1c1ebee205eb1eb784
Khan4218 <mirakber4@gmail.com>
2025-10-14 03:10:57 -0500

    Populate the dropdown

--- 2.Build-full-stack-express-app/express-app/controllers/productsController.js
+++ 2.Build-full-stack-express-app/express-app/controllers/productsController.js
@@ -1,7 +1,25 @@
-export async function getGenres() {
-  console.log('genres');
+import { getDBConnection } from "../db/db.js";
+
+
+const db = await getDBConnection()
+
+export async function getGenres(req, res) {
+
+
+  try {
+
+    const genreRows = await db.all(`SELECT DISTINCT genre FROM products`)
+    const genre = genreRows.map(obj => obj.genre)
+    res.json(genre)
+
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch genres', details: err.message })
+  }
+
 
 }
+
+
 
 export async function getProducts() {
   console.log('products');
