119fcc7842ed73384a6c354012931757cf07c7a7
Khan4218 <mirakber4@gmail.com>
2025-10-15 03:11:08 -0500

    The /register Route

---
+++ 2.Build-full-stack-express-app/express-app/routes/auth.js
@@ -0,0 +1,7 @@
+import { registerUser } from "../controllers/authController.js";
+
+import express from 'express'
+
+export const authRouter = express.Router()
+
+authRouter.post('/register', registerUser)
--- 2.Build-full-stack-express-app/express-app/server.js
+++ 2.Build-full-stack-express-app/express-app/server.js
@@ -1,12 +1,17 @@
 import express from 'express'
 import { productsRouter } from './routes/products.js'
+import { authRouter } from './routes/auth.js'
 const PORT = 8000
 
 const app = express()
 
+app.use(express.json())
 app.use(express.static('public'))
 
 app.use('/api/products', productsRouter)
+
+app.use('/api/auth', authRouter)
+
 
 app.listen(PORT, () => { console.log(`Listening on port ${PORT}`) })
   .on('error', () => {
