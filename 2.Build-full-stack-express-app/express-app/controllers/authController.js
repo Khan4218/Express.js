bb73f502b89a0bdb133098d549605386ba3a4832
Khan4218 <mirakber4@gmail.com>
2025-10-15 04:27:02 -0500

    Validate the User

--- 2.Build-full-stack-express-app/express-app/controllers/authController.js
+++ 2.Build-full-stack-express-app/express-app/controllers/authController.js
@@ -1,4 +1,27 @@
+import validator from 'validator'
 export async function registerUser(req, res) {
-  console.log('req:body', req.body);
+
+  let { name, email, username, password } = req.body
+
+  if (!name || !email || !username || !password) {
+    return res.status(400).json({ error: 'All fields required' })
+  }
+
+  name = name.trim()
+  email = email.trim()
+  username = username.trim()
+  password = password.trim()
+
+  const regixUsername = /^[a-zA-Z0-9_-]{1,20}$/
+
+  if (!regixUsername.test(username)) {
+    return res.status(400).json({ error: 'Invalid characters in username' })
+  }
+
+  if (!validator.isEmail(email)) {
+    return res.status(400).json({ error: 'Ivalid email format' })
+  }
+
+  console.log(req.body);
 
 }
